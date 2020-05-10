912e5f8e2fd8453360d9d4b2050e32e04eb20d81
vaspahomov <37302383+vaspahomov@users.noreply.github.com>
2020-05-10 22:51:24 +0500

    Update Urls.cs

--- backend/Helpers/Urls.cs
+++ backend/Helpers/Urls.cs
@@ -2,6 +2,6 @@ namespace backend.Helpers
 {
     public class Urls
     {
-        public const string Front = "https://photokek.herokuapp.com/";
+        public const string Front = "https://photokek.herokuapp.com";
     }
 }
