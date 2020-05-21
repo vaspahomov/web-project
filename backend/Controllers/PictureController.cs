9d0949380cb1bcf481a09b4df8c1d0abc8583813
romutchio <budljanskiy@mail.ru>
2020-05-21 16:29:42 +0500

    resolved some todos;

--- backend/Controllers/PictureController.cs
+++ backend/Controllers/PictureController.cs
@@ -2,14 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using backend.Data;
+using backend.Data.Entities;
 using backend.Data.Repositories;
 using backend.Services;
 using ImageProcessor.Processors;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace backend.Controllers
 {
@@ -33,21 +36,28 @@ namespace backend.Controllers
         }
 
         [HttpPost("upload")]
-        public async Task<ActionResult<Guid>> UploadFile(IFormFile file)
+        public async Task<ActionResult<PictureEntity>> UploadFile(IFormFile file)
         {
             var picture = GetPictureFromFormFile(file);
             var pictureEntity = await _pictureRepository.Save(picture);
             //TODO: get user from auth info
-            
-            await _userRepository.AddPictureAsync(userId, pictureEntity);
+            var user = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (user != null)
+            {
+                var userId = new Guid(user);
+                await _userRepository.AddPictureAsync(userId, pictureEntity, DateTime.Now);
+            }
+
+            return pictureEntity;
         }
 
         [HttpGet("download/{id}")]
-        public async Task<ActionResult<Picture>> DownloadFile(Guid id)
+        public async Task<ActionResult<Picture>> DownloadFile(string id)
         {
-            var actionResult = await _pictureRepository.Get(id);
-            if (actionResult != null)
-                return actionResult;
+            var entity = new PictureEntity(new ObjectId(id));
+            var picture = await _pictureRepository.Get(entity);
+            if (picture != null)
+                return Ok(picture);
             return NotFound();
         }
 
@@ -60,38 +70,39 @@ namespace backend.Controllers
         }
 
         [HttpPatch("user/{userId}/rotation/{id}")]
-        public async Task<ActionResult<Guid>> Rotate(Guid userId, Guid id, float degrees) =>
+        public async Task<ActionResult<PictureEntity>> Rotate(Guid userId, string id, float degrees) =>
             await ModifyPictureAndSaveForUser(userId, id, pic => _modifier.Rotate(pic, degrees));
 
         [HttpPatch("user/{userId}/text_addition/{id}")]
-        public async Task<ActionResult<Guid>> AddText(Guid userId, Guid id, string text) =>
+        public async Task<ActionResult<PictureEntity>> AddText(Guid userId, string id, string text) =>
             await ModifyPictureAndSaveForUser(userId, id, pic => _modifier.AddText(pic, text));
 
         [HttpPatch("user/{userId}/crop/{id}")]
-        public async Task<ActionResult<Guid>> AddText(Guid userId, Guid id, [FromBody] CropRectangle rectangle) =>
+        public async Task<ActionResult<PictureEntity>> AddText(Guid userId, string id, [FromBody] CropRectangle rectangle) =>
             await ModifyPictureAndSaveForUser(userId, id, pic => _modifier.Crop(pic, rectangle));
 
         [HttpPatch("user/{userId}/gaussian_blur/{id}")]
-        public async Task<ActionResult<Guid>> AddGaussianBlur(Guid userId, Guid id, int size) =>
+        public async Task<ActionResult<PictureEntity>> AddGaussianBlur(Guid userId, string id, int size) =>
             await ModifyPictureAndSaveForUser(userId, id, pic => _modifier.AddGaussianBlur(pic, size));
 
         [HttpPatch("user/{userId}/circular_blur/{id}")]
-        public async Task<ActionResult<Guid>> AddCircularBlur(Guid userId, Guid id) =>
+        public async Task<ActionResult<PictureEntity>> AddCircularBlur(Guid userId, string id) =>
             await ModifyPictureAndSaveForUser(userId, id, _modifier.AddCircularBlur);
 
         [HttpPatch("user/{userId}/sepia_filter/{id}")]
-        public async Task<ActionResult<Guid>> AddSepiaFilter(Guid userId, Guid id) =>
+        public async Task<ActionResult<PictureEntity>> AddSepiaFilter(Guid userId, string id) =>
             await ModifyPictureAndSaveForUser(userId, id, _modifier.AddSepiaFilter);
 
         [HttpPatch("user/{userId}/black_and_white_filter/{id}")]
-        public async Task<ActionResult<Guid>> AddBnWFilter(Guid userId, Guid id) =>
+        public async Task<ActionResult<PictureEntity>> AddBnWFilter(Guid userId, string id) =>
             await ModifyPictureAndSaveForUser(userId, id, _modifier.AddBlackAndWhiteFilter);
 
 
-        private async Task<ActionResult<Guid>> ModifyPictureAndSaveForUser(Guid userId, Guid id, Func<Picture, Picture> f)
+        private async Task<ActionResult<PictureEntity>> ModifyPictureAndSaveForUser(Guid userId, string id, Func<Picture, Picture> f)
         {
             //TODO: add converter between PictureEntity ObjectId and Guid
-            var picture = await _pictureRepository.Get(id);
+            var entity = new PictureEntity(new ObjectId(id));
+            var picture = await _pictureRepository.Get(entity);
             if (picture == null)
                 return NotFound();
             var modified = f(picture);
